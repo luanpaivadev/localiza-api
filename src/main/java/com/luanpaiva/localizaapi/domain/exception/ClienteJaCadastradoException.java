4be3d0b0fda1a46e2ac01fd24bc40520eabd84a1
Luan Paiva <luanpaivadev@gmail.com>
2023-10-01 15:50:41 -0300

    Add validação de cliente já cadastrado

---
+++ src/main/java/com/luanpaiva/localizaapi/domain/exception/ClienteJaCadastradoException.java
@@ -0,0 +1,8 @@
+package com.luanpaiva.localizaapi.domain.exception;
+
+public class ClienteJaCadastradoException extends RuntimeException {
+
+    public ClienteJaCadastradoException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/luanpaiva/localizaapi/domain/service/ClienteService.java
+++ src/main/java/com/luanpaiva/localizaapi/domain/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.luanpaiva.localizaapi.domain.service;
 
+import com.luanpaiva.localizaapi.domain.exception.ClienteJaCadastradoException;
 import com.luanpaiva.localizaapi.domain.exception.ClienteNaoLocalizadoException;
 import com.luanpaiva.localizaapi.domain.model.Cliente;
 import com.luanpaiva.localizaapi.domain.model.Endereco;
@@ -13,6 +14,9 @@ import static java.text.MessageFormat.format;
 
 public class ClienteService implements ClienteServicePort {
 
+    private static final String CLIENTE_JA_POSSUI_CADASTRO = "Cliente já possui cadastro.";
+    private static final String CLIENTE_COM_CPF_0_NAO_LOCALIZADO = "Cliente com CPF {0} não localizado.";
+
     private final ClienteRespositoryPort clienteRespositoryPort;
     private final CepServicePort cepServicePort;
 
@@ -25,11 +29,16 @@ public class ClienteService implements ClienteServicePort {
     public Cliente buscarClientePeloCpf(String cpf) {
         Optional<Cliente> cliente = clienteRespositoryPort.findByCpf(cpf);
         return cliente.orElseThrow(() -> new ClienteNaoLocalizadoException(
-                format("Cliente com CPF {0} não localizado.", cpf)));
+                format(CLIENTE_COM_CPF_0_NAO_LOCALIZADO, cpf)));
     }
 
     @Override
     public Cliente salvarCliente(Cliente cliente, String cep) {
+
+        clienteRespositoryPort.findByCpf(cliente.getCpf()).ifPresent(c -> {
+            throw new ClienteJaCadastradoException(CLIENTE_JA_POSSUI_CADASTRO);
+        });
+
         Endereco endereco = cepServicePort.consultarEnderecoPorCep(cep);
         cliente.setEndereco(endereco);
         return clienteRespositoryPort.save(cliente);
