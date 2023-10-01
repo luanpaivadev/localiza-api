fa9313269be8c1700c338b298cb927c8221c6a29
Luan Paiva <luanpaivadev@gmail.com>
2023-10-01 18:17:54 -0300

    Update ReservaRepositoryJpa.java

--- src/main/java/com/luanpaiva/localizaapi/adapter/output/repository/jpa/ReservaRepositoryJpa.java
+++ src/main/java/com/luanpaiva/localizaapi/adapter/output/repository/jpa/ReservaRepositoryJpa.java
@@ -3,6 +3,7 @@ package com.luanpaiva.localizaapi.adapter.output.repository.jpa;
 import com.luanpaiva.localizaapi.adapter.output.repository.jpa.entities.ReservaEntity;
 import com.luanpaiva.localizaapi.domain.model.StatusReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface ReservaRepositoryJpa extends JpaRepository<ReservaEntity, Long> {
 
+    @Query("FROM ReservaEntity r JOIN FETCH r.veiculo JOIN FETCH r.cliente c JOIN FETCH c.endereco WHERE r.statusReserva = ?1")
     List<ReservaEntity> findByStatusReserva(StatusReserva statusReserva);
 }
